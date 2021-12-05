9319cfa78528b55dc44f79a060316fdca1340e76
Felipe Gabriel <liperesende1@hotmail.com>
2021-12-04 22:15:46 -0300

    Ajustes

--- src/database/create-database.sql
+++ src/database/create-database.sql
@@ -2,6 +2,7 @@ CREATE DATABASE trafico;
 
 CREATE TABLE trafico.feedbacks (
 	id int(10) PRIMARY KEY auto_increment,
+	name varchar(200),
 	email varchar(200),
 	message varchar(3000)
 	create_at TIMESTAMP  
